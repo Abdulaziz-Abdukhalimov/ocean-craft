cd2b77ba3aa8ebf2e3eb4be90f17da733921a30f
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2025-11-21 19:43:26 +0900

    feat: configure environmental varibales

--- apps/ocean-craft-batch/src/ocean-craft-batch.module.ts
+++ apps/ocean-craft-batch/src/ocean-craft-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OceanCraftBatchController } from './ocean-craft-batch.controller';
 import { OceanCraftBatchService } from './ocean-craft-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [OceanCraftBatchController],
   providers: [OceanCraftBatchService],
 })
