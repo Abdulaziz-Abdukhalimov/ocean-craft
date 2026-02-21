4f58d9b23fd283df4b987c4ba72ba7b564f45c0c
Abdulaziz Abdukhalimov <abdukhalimov_front_end@Abdulazizs-MacBook-Pro.local>
2026-02-22 04:15:00 +0900

    fix: change ports into final prod port

--- apps/ocean-craft/src/socket/notification.gateway.ts
+++ apps/ocean-craft/src/socket/notification.gateway.ts
@@ -12,7 +12,7 @@ import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
 	cors: {
-		origin: 'http://localhost:3000',
+		origin: 'http://localhost:4001',
 		credentials: true,
 	},
 	namespace: 'notifications',
